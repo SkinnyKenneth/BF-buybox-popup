5a7ac85babc9db35a9df3461e41dea3353c87839
SkinnyKenneth <134853391+SkinnyKenneth@users.noreply.github.com>
2023-10-05 10:12:00 +0200

    added jquery

--- main.js
+++ main.js
@@ -1,31 +1,60 @@
-// changing button styles on select
-const selectBtns = document.querySelectorAll(".value-button");
+jQuery(function ($) {
+    // changing button styles on select
+    const selectBtns = $(".value-button");
+  
+    selectBtns.on("click", function () {
+      selectBtns.removeClass("selected");
+      $(this).addClass("selected");
+    });
+  
+    // switching tier options based on value select
+    const subOptions = $(".sub");
+  
+    selectBtns.on("click", function () {
+      subOptions.removeClass("selected");
+      subOptions.eq(selectBtns.index(this)).addClass("selected");
+    });
+  
+    // update subscription title on click
+    const subHeader = $(".sub-container > h2");
+    const selectedValue = $(".value-button.selected");
+  
+    subHeader.text(selectedValue.text());
+  
+    selectBtns.on("click", function () {
+      subHeader.text($(this).text());
+    });
+  });
+  
 
-selectBtns.forEach(btn => {
-    btn.addEventListener("click", (e) => {
-        selectBtns.forEach(btn => btn.classList.remove("selected"));
-        e.target.classList.add("selected");
-    })
-})
+// // changing button styles on select
+// const selectBtns = document.querySelectorAll(".value-button");
 
-// switching tier options based on value select
-const subOptions = document.querySelectorAll(".sub");
+// selectBtns.forEach(btn => {
+//     btn.addEventListener("click", (e) => {
+//         selectBtns.forEach(btn => btn.classList.remove("selected"));
+//         e.target.classList.add("selected");
+//     })
+// })
 
-selectBtns.forEach((btn, idx) => {
-    btn.addEventListener("click", (e) => {
-        subOptions.forEach(option => option.classList.remove("selected"));
-        subOptions[idx].classList.add("selected");
-    })
-})
+// // switching tier options based on value select
+// const subOptions = document.querySelectorAll(".sub");
 
-// update subscription title on click
-const subHeader = document.querySelector(".sub-container > h2");
-const selectedValue = document.querySelector(".value-button.selected")
+// selectBtns.forEach((btn, idx) => {
+//     btn.addEventListener("click", (e) => {
+//         subOptions.forEach(option => option.classList.remove("selected"));
+//         subOptions[idx].classList.add("selected");
+//     })
+// })
 
-subHeader.innerText = selectedValue.innerText;
+// // update subscription title on click
+// const subHeader = document.querySelector(".sub-container > h2");
+// const selectedValue = document.querySelector(".value-button.selected")
 
-selectBtns.forEach(btn => {
-    btn.addEventListener("click", (e) => {
-        subHeader.innerText = e.target.innerText;
-    })
-})
+// subHeader.innerText = selectedValue.innerText;
+
+// selectBtns.forEach(btn => {
+//     btn.addEventListener("click", (e) => {
+//         subHeader.innerText = e.target.innerText;
+//     })
+// })
